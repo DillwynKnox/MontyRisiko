41a676e608b6b9353e11f28028ba2a501699333d
DillwynKnox <anton.trappe@gmx.de>
2023-04-15 22:54:56 +0200

    chess seems to work

--- chessSit.py
+++ chessSit.py
@@ -1,33 +1,32 @@
 from math import sqrt, log, inf
 import random
 from montecarlo import Node,Montycarlo
-from chessboard import Board
 import chess
 
 class ChessNode(Node):
-    def __init__(self, board, player,level=0):
-        self.board = Board(board)
+    def __init__(self, board:chess.Board, player,level=0):
+        self.board = board
         self.player = player
         self.level=level
 
     def make_children(self):
         children = []
-        moves = self.board.get_all_moves(self.player)
+        moves = self.board.legal_moves
         for move in moves:
             child_board = self.board.copy()  # Create a copy of the board for the child node
-            child_board.make_move(move)
-            child_player = self.board.get_next_player(self.player)
+            child_board.push(move)
+            child_player = (self.player+1) % 2
             child_node = ChessNode(child_board, child_player,self.level+1)
             children.append(child_node)
         return children
 
     def get_random_child(self):
         child_board = self.board.copy()
-        moves = child_board.get_all_moves(self.player)
+        moves = list(child_board.legal_moves)
         move = random.choice(moves)
         
-        child_board.make_move(move)
-        child_player = self.board.get_next_player(self.player)
+        child_board.push(move)
+        child_player = self.board.child_player = (self.player+1) % 2
         return ChessNode(child_board, child_player,self.level+1)
 
     def get_player(self):
@@ -41,15 +40,39 @@ class ChessNode(Node):
     def get_score(self):
         if self.level>=1000:
             return [0.5,0.5]
-        scores = self.board.get_scores()
+        scores = [0.5, 0.5]  # Assuming draw by default
+        if self.board.is_checkmate():
+            scores[self.get_winner()] = 1.0
+            scores[self.get_loser()] = 0.0
+        elif self.board.is_stalemate() or self.board.is_insufficient_material() \
+                or self.board.is_seventyfive_moves() or self.board.is_fivefold_repetition() \
+                or self.board.is_fifty_moves():
+            pass  # Draw, scores remain [0.5, 0.5]
         return scores
+    
+    def get_winner(self):
+        result = self.board.result()
+        if result == '1-0':
+            return 0
+        elif result == '0-1':
+            return 1
+        else:
+            return None
+
+    def get_loser(self):
+        winner = self.get_winner()
+        if winner is not None:
+            return 1 - winner
+        else:
+            return None
+
 
 def getBoard(Board):
     sit=ChessNode(Board,0)
     monty=Montycarlo(2,sit)
-    for _ in range(1000):
+    for _ in range(100):
         monty.simulate()
-    return monty.getEndChild.myNode.board
+    return monty.getEndChild().myNode.board
 
 if __name__=="__main__":
     print(getBoard(chess.Board()))
