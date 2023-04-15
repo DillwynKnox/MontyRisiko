75c02763f1be408e35a1200a4fd83b5d39a9356e
DillwynKnox <anton.trappe@gmx.de>
2023-04-15 12:29:34 +0200

    added get recruit

--- gameUtil_test.py
+++ gameUtil_test.py
@@ -1,33 +1,38 @@
 import unittest
 import pandas as pd
-from game_utils import possible_Attacks,createGame
+from game_utils import possible_Attacks,createGame,get_recruitment
 
 
 
 
 
 class TestGameUtils(unittest.TestCase):
-    def test_createGame(self):
-        testgame=createGame()
-        self.assertEqual(len(testgame["countries"].keys()),8)
-
-    def test_possibleAttacks(self):
-        exampleGame=createGame()
+    def __init__(self, methodName: str = "runTest") -> None:
+        super().__init__(methodName)
+        self.exampleGame=createGame()
         troups=[1]*8
         troups[1]=4
         exampleCountries=pd.DataFrame(
-            {"name": exampleGame["countries"].keys(),
+            {"name": self.exampleGame["countries"].keys(),
             "owner": [0,0,1,0,0,2,1,2],
             "border":[False,True,True,True,True,True,True,True],
             "troups":troups},
             index=[0,1,2,3,4,5,6,7])
-        exampleSit={
+        self.exampleSit={
             "countries":exampleCountries,
             "turn":0
         }
-        self.assertEqual(possible_Attacks(exampleSit,exampleGame),[("NRW","Preussen")])
 
+    def test_createGame(self):
+        testgame=createGame()
+        self.assertEqual(len(testgame["countries"].keys()),8)
+        self.assertEqual(len(testgame["players"].keys()),3)
 
+    def test_possibleAttacks(self):
+        self.assertEqual(possible_Attacks(self.exampleSit,self.exampleGame),[("NRW","Preussen")])
+
+    def test_recruit(self):
+        self.assertEqual(get_recruitment(self.exampleSit,self.exampleGame),7)
 
 
 if __name__== '__main__':
--- game_utils.py
+++ game_utils.py
@@ -1,11 +1,11 @@
 import json
 import pandas as pd
 
-def createGame(countrypath='gameInfo/countries.json'):
-    with open(countrypath) as gamefile:
-        countries=json.load(gamefile)
-    Game={}
-    Game["countries"]=countries
+def createGame(gamepath='gameInfo/testgame.json'):
+    with open(gamepath) as gamefile:
+        Game=json.load(gamefile)
+    for cont in Game["continents"].keys():
+        Game["continents"][cont]["members"]=set(Game["continents"][cont]["members"])
     return Game
 
 def possible_Attacks(sit:dict,game:dict):
@@ -24,3 +24,20 @@ def possible_Attacks(sit:dict,game:dict):
             if nei not in my_Areas['name'].tolist():
                 ac.append((area['name'],nei))
     return ac
+
+def get_recruitment(sit:dict,game:dict):
+    gain=game["basegain"]
+    cou_df : pd.DataFrame=sit["countries"]
+    player=sit["turn"]
+    
+    #area gain
+    my_Areas=cou_df.loc[cou_df["owner"]==player]
+    my_Areas=set(my_Areas["name"].tolist())
+    gain+=game["tgain"][len(my_Areas)]
+
+    #continent gain
+    for cont in game["continents"].keys():
+        if game["continents"][cont]["members"].issubset(my_Areas):
+            gain+=game["continents"][cont]["gain"]
+
+    return gain
