10604e8d89adf84eebcec5aa91c5b64de19009e8
DillwynKnox <anton.trappe@gmx.de>
2023-04-15 10:47:00 +0200

    added possible Attacks

--- game_utils.py
+++ game_utils.py
@@ -6,5 +6,19 @@ def get_Countries():
         countries=json.load(gamefile)
     return countries
 
-def possible_Attacks():
-    pass
+def possible_Attacks(sit:dict,game:dict):
+    cou_df : pd.DataFrame=sit["countries"]
+    player=sit["turn"]
+    ac=[]
+
+    #get bordering countries with enough troups
+    my_Areas=cou_df.loc[cou_df["owner"]==player]
+    possibleAreas=my_Areas.loc[(my_Areas['border']==True) & (my_Areas['troups']>1)]
+
+
+    #get the neighbouring countries
+    for _,area in possibleAreas.iterrows():
+        for nei in game['countries'][area['name']]:
+            if nei not in my_Areas['name'].tolist():
+                ac.append(area['name'],area)
+    return ac
